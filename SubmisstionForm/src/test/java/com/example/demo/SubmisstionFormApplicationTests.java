28e9ca4deef23f3c7edd2857f6b4413d6fcb2e33
divyapatel625 <61935781+divyapatel625@users.noreply.github.com>
2025-01-25 21:16:43 +0530

    Update SubmisstionFormApplicationTests.java

--- SubmisstionForm/src/test/java/com/example/demo/SubmisstionFormApplicationTests.java
+++ SubmisstionForm/src/test/java/com/example/demo/SubmisstionFormApplicationTests.java
@@ -6,6 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SubmisstionFormApplicationTests {
 
+	public void test() {
+		//test comment
+	}
 	@Test
 	void contextLoads() {
 	}
