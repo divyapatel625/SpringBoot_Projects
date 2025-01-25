298e186263d61ca3f1fa240f107e960261d20759
Divya Sanket Patel <divya.patel0625@gmail.com>
2025-01-25 20:51:08 +0530

    Added new POST service for user

--- restfull-web-services/src/main/java/com/springboot/rest/webservice/restfull_web_services/User/UserController.java
+++ restfull-web-services/src/main/java/com/springboot/rest/webservice/restfull_web_services/User/UserController.java
@@ -1,10 +1,16 @@
 package com.springboot.rest.webservice.restfull_web_services.User;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserController {
@@ -25,4 +31,15 @@ public class UserController {
 	public User GetUser(@PathVariable int id) {
 		return service.findUserById(id);
 	}
+
+	@PostMapping("/users")
+	public ResponseEntity<Object> CreateUser(@RequestBody User user) {
+		User savedUser = service.saveUser(user);
+		
+		 URI location = ServletUriComponentsBuilder.fromCurrentRequest().
+				 path("/{id}").
+				 buildAndExpand(savedUser.getId()).toUri();
+		return ResponseEntity.created(location).build();
+	}
+
 }
--- restfull-web-services/src/main/java/com/springboot/rest/webservice/restfull_web_services/User/UserDaoService.java
+++ restfull-web-services/src/main/java/com/springboot/rest/webservice/restfull_web_services/User/UserDaoService.java
@@ -11,15 +11,16 @@ import org.springframework.stereotype.Component;
 @Component 
 class UserDaoService {
 
-	
+	public static int usercount=0;
 	private static List<User> users = new  ArrayList<User>();
 	static {
-		users.add(new User(1, "James", LocalDate.now().minusYears(20)));
-		users.add(new User(2, "Mark", LocalDate.now().minusYears(25)));
-		users.add(new User(3, "Scott", LocalDate.now().minusYears(30)));
-		users.add(new User(4, "Scott", LocalDate.now().minusYears(27)));
-		users.add(new User(5, "Scott", LocalDate.now().minusYears(32)));
+		users.add(new User(++usercount, "James", LocalDate.now().minusYears(20)));
+		users.add(new User(++usercount, "Mark", LocalDate.now().minusYears(25)));
+		users.add(new User(++usercount, "Scott", LocalDate.now().minusYears(30)));
+		users.add(new User(++usercount, "Sam", LocalDate.now().minusYears(27)));
+		users.add(new User(++usercount, "Jay", LocalDate.now().minusYears(32)));
 	}
+	
 	
 	public List<User> findAll() {
 		return users;
@@ -30,5 +31,11 @@ class UserDaoService {
 		Predicate<? super User> Predicate = user -> user.getId().equals(id);
 		return users.stream().filter(Predicate).findFirst().get();
 				}
+
+	public User saveUser(User user) {
+		user.setId(++usercount);
+		users.add(user);
+		return user ;
+	}
 	
 }
